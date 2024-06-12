e1648c7341caa5b89d1541f878d89fa137966043
Esteban S <esteban.solis.alfaro@gmail.com>
2024-06-11 20:49:54 -0600

    Primitive menu and main for frontend

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+// main.cpp
+#include <SFML/Graphics.hpp>
+#include "MenuUI.hpp"
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Menu de Inicio", sf::Style::Titlebar | sf::Style::Close);
+    //for static window
+
+    try {
+        Menu menu(window);
+        menu.run();
+    } catch (const std::runtime_error &e) {
+        std::cerr << e.what() << std::endl;
+        return -1;
+    }
+
+    return 0;
+}
